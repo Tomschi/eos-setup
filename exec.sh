705b5ba1d64f145063b5254c7d74b8d71218b46e
Tomschi <codingtomschi@gmail.com>
2026-01-02 17:10:09 +0100

    Move package files to eos-packages.

--- exec.sh
+++ exec.sh
@@ -10,10 +10,13 @@ for script in "$EOS_SETUP_DIR"/"01-pre"/*.sh; do
   "$script"
 done
 
-for script in "$EOS_SETUP_DIR"/"02-pacman"/*.sh; do
-  "$script"
-done
+# clone repositories
+# TODO: final version should use main branch
+git clone -b intitial github.com:Tomschi/dotfiles.git ~/.local/share/dotfiles
+git clone -b intitial github.com:Tomschi/dotfiles-kde.git ~/.local/share/dotfiles-kde
+git clone -b intitial github.com:Tomschi/eos-files.git ~/.local/share/eos-files
+git clone -b intitial github.com:Tomschi/eos-packages.git ~/.local/share/eos-packages
 
-for script in "$EOS_SETUP_DIR"/"03-aur"/*.sh; do
-  "$script"
-done
+# TODO: remove, only for script development
+git clone -b intitial github.com:Tomschi/dotfiles-kde-orig.git ~/.local/share/dotfiles-kde-orig
+git clone -b intitial github.com:Tomschi/eos-files-orig.git ~/.local/share/eos-files-orig
